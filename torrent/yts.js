6271fd9e29864916fdaead5228066f04d2a8a446
Ritu Raj Pratap Singh <iamriturajps@gmail.com>
2025-01-26 17:36:35 +0530

    fix: update User-Agent string for torrent search functions to latest version

--- torrent/yts.js
+++ torrent/yts.js
@@ -13,7 +13,7 @@ async function torrentYts(query, page = '1') {
 	let html;
 	try {
 		html = await axios.get(url, headers = {
-			"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.106 Safari/537.36"
+			"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/112.0.5615.121 Safari/537.36"
 		});
 	} catch {
 		return null;
