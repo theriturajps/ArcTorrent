5071ff5d1150fb4730d1d93f13b01d3b88bbc8c1
Ritu Raj Pratap Singh <iamriturajps@gmail.com>
2025-01-27 12:37:26 +0530

    refactor: rename index router file and migrate its content to apiIndexRoutes

--- src/app.js
+++ src/app.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const path = require('path');
 const cors = require('cors');
-const apiIndexRouter = require('./routes/index');
+const apiIndexRouter = require('./routes/apiIndexRoutes');
 
 const app = express();
 
