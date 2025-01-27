c64c6e1ef26ec97f9b534384c1b80b554fa01b3b
Ritu Raj Pratap Singh <iamriturajps@gmail.com>
2025-01-27 12:05:24 +0530

    refactor: migrate application structure and update dependencies for improved organization

--- src/routes/torrent.routes.js
+++
@@ -1,7 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const torrentController = require('../controllers/torrent.controller');
-
-router.get('/:website/:query/:page?', torrentController.search);
-
-module.exports = router;
