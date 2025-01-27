5358345203852bf279f0fcd1297947f63a203912
Ritu Raj Pratap Singh <iamriturajps@gmail.com>
2025-01-27 12:58:23 +0530

    fix: update static file serving paths to correctly reference the public directory

--- src/app.js
+++ src/app.js
@@ -6,14 +6,14 @@ const apiIndexRouter = require('./routes/apiIndexRoutes');
 const app = express();
 
 app.use(cors());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, '..' ,'public')));
 
 // Routes
 app.use('/', apiIndexRouter);
 
 // Serve index.html for root route
 app.get('/', (req, res) => {
-	res.sendFile(path.join(__dirname, 'public', 'index.html'));
+	res.sendFile(path.join(__dirname,'..' ,'public', 'index.html'));
 });
 
 const PORT = process.env.PORT || 3000;
