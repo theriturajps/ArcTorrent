b42ee3f65c61dfff69fb0006678080d555908257
Ritu Raj Pratap Singh <iamriturajps@gmail.com>
2025-01-28 15:50:03 +0530

    refactor: rename torrentXprimehub function to limeTorrent and update URL fetching logic

--- src/torrent/xprimehub.js
+++ src/torrent/xprimehub.js
@@ -1,34 +1,24 @@
-const cheerio = require('cheerio');
-const axios = require('axios');
+const cheerio = require('cheerio')
+const axios = require('axios')
 
-async function torrentXprimehub(query, page = 1) { // Default page is 1 if not specified in the arguments
-	const ALLURL = [];
-	const url = `https://xprimehub.vip/page/${page}/?s=${query}`; // https://xprimehub.vip/page/2/?s=download
-
+async function limeTorrent(query, page = '1') {
+	var ALLTORRENT = [];
+	const url = `https://xprimehub.vip/page/${page}/?s=${query}`;
 	let html;
 	try {
-		html = await axios.get(url, {
-			headers: {
-				"User-Agent":
-					"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/112.0.5615.121 Safari/537.36",
-			},
+		html = await axios.get(url, headers = {
+			"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/112.0.5615.121 Safari/537.36"
 		});
-	} catch (error) {
-		console.error("Error fetching data:", error.message);
-		return null; // Return null if there's an error
+
+	} catch {
+		return null;
 	}
 
-	const $ = cheerio.load(html.data);
+	const $data = cheerio.load(html.data);
 
-	// // Extract URLs from the specified elements
-	// $('div.bw_title').each((_, element) => {
-	// 	const url = $(element).find('a');
-	// 	ALLURL.push(url);
-	// });
+	ALLTORRENT.push($data)
 
-	ALLURL.push($) // Push the entire page to the array
+	return ALLTORRENT;
 
-	return ALLURL; // Return the array of URLs
 }
-
-module.exports = torrentXprimehub;
+module.exports = limeTorrent
