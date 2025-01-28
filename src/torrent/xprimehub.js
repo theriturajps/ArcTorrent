9b26d954fc8c7399fb56f8e76ad1644a50542eb8
Ritu Raj Pratap Singh <iamriturajps@gmail.com>
2025-01-28 15:55:07 +0530

    refactor: improve error handling and code formatting in torrentXprimehub function

--- src/torrent/xprimehub.js
+++ src/torrent/xprimehub.js
@@ -1,24 +1,31 @@
-const cheerio = require('cheerio')
-const axios = require('axios')
+const cheerio = require('cheerio');
+const axios = require('axios');
 
 async function torrentXprimehub(query, page = '1') {
 	var ALLTORRENT = [];
 	const url = `https://xprimehub.vip/page/${page}/?s=${query}`;
 	let html;
-	try {
-		html = await axios.get(url, headers = {
-			"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/112.0.5615.121 Safari/537.36"
-		});
 
-	} catch {
+	try {
+		html = await axios.get(url, {
+			headers: {
+				"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/112.0.5615.121 Safari/537.36"
+			}
+		});
+	} catch (error) {
+		console.error("Error fetching the URL:", error);
 		return null;
 	}
 
-	const $data = cheerio.load(html.data);
+	const $ = cheerio.load(html.data);
 
-	ALLTORRENT.push($data)
+	$('.bw_title').each((index, element) => {
+		const link = $(element).find('a').attr('href');
+
+		ALLTORRENT.push({ link });
+	});
 
 	return ALLTORRENT;
-
 }
-module.exports = torrentXprimehub
+
+module.exports = torrentXprimehub;
