1d28733cbc3f32e0ce1a21200fb85e7c59820acb
Ritu Raj Pratap Singh <iamriturajps@gmail.com>
2025-01-28 15:09:50 +0530

    refactor: replace DOMParser with Cheerio for improved HTML parsing in torrentVegaMovies function

--- src/torrent/vegaMoviesFile.js
+++ src/torrent/vegaMoviesFile.js
@@ -1,6 +1,7 @@
+const cheerio = require('cheerio');
 const axios = require('axios');
 
-async function torrentVegaMovies(query, page = 1) { // Default page is 1 if not specified in the arguments
+async function torrentVegaMovies(query, page = 1) {
 	const ALLURL = [];
 	const url = `https://vegamovies.ms/page/${page}?s=${query}`;
 
@@ -8,29 +9,22 @@ async function torrentVegaMovies(query, page = 1) { // Default page is 1 if not 
 	try {
 		html = await axios.get(url, {
 			headers: {
-				"User-Agent":
-					"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/112.0.5615.121 Safari/537.36",
+				"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/112.0.5615.121 Safari/537.36",
 			},
 		});
 	} catch (error) {
 		console.error("Error fetching data:", error.message);
-		return null; // Return null if there's an error
+		return null;
 	}
 
-	const data = html.data;
-	const parser = new DOMParser();
-	const document = parser.parseFromString(data, "text/html");
+	const $ = cheerio.load(html.data);
 
-	// Extract URLs from the specified elements
-	const elements = document.querySelectorAll('div.post-hover a');
-	elements.forEach((element) => {
-		const url = element.href;
-		if (url) {
-			ALLURL.push(url);
-		}
+	$('#post-179226').each((_, element) => {
+		const url = $(element).find('a').attr('href');
+		ALLURL.push(url);
 	});
 
-	return ALLURL; // Return the array of URLs
+	return ALLURL;
 }
 
 module.exports = torrentVegaMovies;
