3407904e1f832c308c9e25634acc77e2d6be2d0d
Ritu Raj Pratap Singh <iamriturajps@gmail.com>
2025-01-28 14:04:00 +0530

    refactor: modify torrentVegaMovies function to return extracted URLs and comment out data extraction logic

--- src/torrent/vegaMoviesFile.js
+++ src/torrent/vegaMoviesFile.js
@@ -23,34 +23,33 @@ async function torrentVegaMovies(query, page = '1') {
 	$('div.post-inner.post-hover').each((_, element) => {
 		let url = $(element).find('a').attr('href');
 		ALLURL.push(url);
-		console.log(ALLURL);
 	})
 
-	await Promise.all(ALLURL.map(async (url) => {
-		const data = {
-			'Name': null,
-			'Likes': null,
-			'Language': null,
-			'Url': null,
-		};
-		let html;
-		try {
-			html = await axios.get(url);
-		} catch {
-			return;
-		}
+	// await Promise.all(ALLURL.map(async (url) => {
+	// 	const data = {
+	// 		'Name': null,
+	// 		'Likes': null,
+	// 		'Language': null,
+	// 		'Url': null,
+	// 	};
+	// 	let html;
+	// 	try {
+	// 		html = await axios.get(url);
+	// 	} catch {
+	// 		return;
+	// 	}
 
-		const $ = cheerio.load(html.data);
+	// 	const $ = cheerio.load(html.data);
 
-		data['Name'] = $('div.post-inner.group').find('h1').text();
-		data['Likes'] = $('div.bottom-info div.rating-row').eq(0).find('span').eq(1).text()
-		data['Language'] = $('#content > div:nth-child(2) > article > div > div.entry.themeform > div.entry-inner > p:nth-child(5) > span:nth-child(10) > strong').text().trim();
-		data['Url'] = url;
+	// 	data['Name'] = $('div.post-inner.group').find('h1').text();
+	// 	data['Likes'] = $('div.bottom-info div.rating-row').eq(0).find('span').eq(1).text()
+	// 	data['Language'] = $('#content > div:nth-child(2) > article > div > div.entry.themeform > div.entry-inner > p:nth-child(5) > span:nth-child(10) > strong').text().trim();
+	// 	data['Url'] = url;
 
-		all.push(data);
-	}))
+	// 	all.push(data);
+	// }))
 
-	return all;
+	return ALLURL;
 
 }
 
